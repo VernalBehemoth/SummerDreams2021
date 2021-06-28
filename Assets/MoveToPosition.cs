eef4fda47e55e0773e95f35fba2a6124ba72142a
Graham <gvoisey@live.com>
2021-06-28 21:57:52 +0200

    Added cinemachine and started initial scene

---
+++ Assets/MoveToPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPosition : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        speed += 0.003F;
+    }
+}
